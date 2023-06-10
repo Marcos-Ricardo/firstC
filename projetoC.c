90f317f1bb7baeb8c54e2d36c32938790c0c67cb
marcos <marcos.itajui@gmail.com>
2023-06-10 15:36:59 -0300

    Changed hello world to name and age inputs.

--- projetoC.c
+++ projetoC.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    char name[50];
+    int idade;
 
-    printf("Hello world!\n");
+    printf("\nDigite o seu nome: ");
+    scanf("%s", name);
 
+    printf("\nSua idade: ");
+    scanf("%i", &idade);
 
+    system("cls");
+
+    printf("\nSeu nome eh %s", name);
+    printf("\nVoce tem %ianos", idade );
 
     return 0;
 }
-
-
